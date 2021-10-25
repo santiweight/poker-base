9bbf0ada992df276b6d5602cfa226bc41bf11cf8
Santi Weight <santiagoweight@forwardnetworks.com>
2021-10-24 17:37:04 -0700

    update tests

--- test/Test/Poker/Game.hs
+++ test/Test/Poker/Game.hs
@@ -7,7 +7,8 @@ import Test.Hspec
 
 spec_allPositions :: SpecWith ()
 spec_allPositions = do
-  it "heads up all positions" $ (positionToTxt TwoPlayers <$> allPositions TwoPlayers) `shouldBe` ["BU", "BB"]
-  it "3max all positions" $ (positionToTxt ThreePlayers <$> allPositions ThreePlayers) `shouldBe` ["BU", "SB", "BB"]
-  it "4max all positions" $ (positionToTxt FourPlayers <$> allPositions FourPlayers) `shouldBe` ["CO", "BU", "SB", "BB"]
-  it "9max all positions" $ (positionToTxt NinePlayers <$> allPositions NinePlayers) `shouldBe` ["UTG", "UTG1", "UTG2", "LJ", "HJ", "CO", "BU", "SB", "BB"]
+  it "heads up all positions" $ allPositions TwoPlayers `shouldBe` [BU, BB]
+  it "3max all positions" $ allPositions ThreePlayers `shouldBe` [BU, SB, BB]
+  it "4max all positions" $ allPositions FourPlayers `shouldBe` [UTG, BU, SB, BB]
+  it "9max all positions" $ allPositions NinePlayers `shouldBe` [UTG, UTG1, UTG2, UTG3, UTG4, UTG5, BU, SB, BB]
+  it "10max all positions" $ allPositions TenPlayers `shouldBe` [UTG, UTG1, UTG2, UTG3, UTG4, UTG5, UTG6, BU, SB, BB]
